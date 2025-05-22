88b8cf3b1e5f58d6cf06e3ef7c570b9031fd0b4f
Jan Busson <janbusson@gmx.de>
2025-05-22 20:56:27 +0200

    Added a Button to go back and edited the Buttons so the allign nicely in the layout

--- app/src/main/java/com/example/greendr/LoginView.java
+++ app/src/main/java/com/example/greendr/LoginView.java
@@ -1,6 +1,9 @@
 package com.example.greendr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginView extends AppCompatActivity {
@@ -8,5 +11,14 @@ public class LoginView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_view);
+
+        //speichert den Button aus dem ausgewähltem Layout
+        Button openSecondBtn = findViewById(R.id.go_back_start);
+
+        //Intent object für den Bildschirmwechsel
+        openSecondBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginView.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/greendr/MainActivity.java
+++ app/src/main/java/com/example/greendr/MainActivity.java
@@ -12,8 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //speichert den Button aus dem ausgewähltem Layout
         Button openSecondBtn = findViewById(R.id.button_open_second);
 
+        //Intent object für den Bildschirmwechsel
         openSecondBtn.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, LoginView.class);
             startActivity(intent);
