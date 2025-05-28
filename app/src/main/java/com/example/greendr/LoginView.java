4db4b9e071e468d4526c3152cb46e5fbe1b36891
Jan Busson <janbusson@gmx.de>
2025-05-28 12:07:11 +0200

    DB Connection

--- app/src/main/java/com/example/greendr/LoginView.java
+++ app/src/main/java/com/example/greendr/LoginView.java
@@ -2,8 +2,15 @@ package com.example.greendr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /*########################################################
 LoginView ermöglicht das eingeben der Login Daten und prüft deren Korrektheit
@@ -21,6 +28,20 @@ public class LoginView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_view);
+
+        Log.d("FIREBASE", "StartView gestartet!");
+
+        // Firebase Referenz holen
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("test");
+
+        // Testdaten
+        Map<String, Object> testData = new HashMap<>();
+        testData.put("name", "Anna");
+        testData.put("age", 22);
+
+        // Daten an Firebase senden
+        ref.setValue(testData);
 
         //speichert den Button aus dem ausgewähltem Layout
         Button openSecondBtn = findViewById(R.id.go_back_start);
